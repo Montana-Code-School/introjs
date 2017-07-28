a3acc65d4f2ab6d33de7440abe73f8ee8c922bc7
hajush <harold@shinsato.com>
2017-07-28 15:08:45 -0600

    add back lost solutions from 2016 class

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,8 +1,30 @@
-function TicTacToe() {
-  this.set = function(horizontal, vertical) {
+class TicTacToe {
+
+  constructor() {
+    this.grid = [
+      ["_", "_", "_"],
+      ["_", "_", "_"],
+      [" ", " ", " "]
+    ];
+  }
+
+  set(xcord, ycord, mark) {
+    this.grid[xcord - 1][ycord - 1] = mark;
   };
-  this.get = function(horizontal, vertical) {
+
+  get(xcord, ycord) {
+    return this.grid[xcord - 1][ycord - 1];
   };
-}
+
+  displayGrid() {
+    var result = '';
+    for(var i = 0; i < this.grid.length; i++){
+      result += this.grid[i][0] + "|"
+        + this.grid[i][1] + "|"
+        + this.grid[i][2] + "\n";
+    };
+    return result;
+  };
+};
 
 module.exports = TicTacToe;
--- ticTacToeTest.js
+++ ticTacToeTest.js
@@ -1,23 +1,60 @@
 var assert = require('chai').assert;
 var TicTacToe = require('./ticTacToe');
 
-xdescribe("Tic Tac Toe", function() {
+describe("Tic Tac Toe", function() {
   it("can place an x at 1, 1", function() {
     //Arrange
     var ticTacToe = new TicTacToe();
     //Act
     ticTacToe.set(1, 1, "X");
-    var atOneOne = ticTacToe.get(1, 1, "X");
     //Assert
-    assert.equal(atOneOne, "X");
+    assert.equal(ticTacToe.get(1, 1), "X");
   });
 
-  xit("can place an o at 1, 1", function() {});
-  xit("can place an x at 1, 1, and o at 2,2", function() {});
-  xit("can place an x at 1,1 2,2 and 3,3", function() {});
-  xit("can make a 3 line string version of an empty grid", function() {});
-  xit("can make a 3 line string of a grid with an x in the center (2,2)", function() {});
-  xit("can show x wins when x is at 1,1 1,2 and 1,3", function() {});
-  xit("can show o wins when o is at 1,1 2,1 and 3,1", function() {});
-  xit("can show x wins when x is in the diagonal", function() {});
+  it("can place an o at 3, 3", function() {
+    var ticTacToe = new TicTacToe();
+    ticTacToe.set(3, 3, "O");
+    assert.equal(ticTacToe.get(3, 3), "O");
+  });
+
+  it("can place an x at 1, 1, and o at 2,2", function() {
+    var ticTacToe = new TicTacToe();
+    ticTacToe.set(1, 1, "X");
+    assert.equal(ticTacToe.get(1, 1), "X");
+    ticTacToe.set(2, 2, "O");
+    assert.equal(ticTacToe.get(2, 2), "O");
+  });
+
+  it("can place an x at 1,1 2,2 and 3,3", function() {
+    var ticTacToe = new TicTacToe();
+    ticTacToe.set(1, 1, "X");
+    assert.equal(ticTacToe.get(1, 1), "X");
+    ticTacToe.set(2, 2, "X");
+    assert.equal(ticTacToe.get(2, 2), "X");
+    ticTacToe.set(3, 3, "X");
+    assert.equal(ticTacToe.get(3, 3), "X");
+  });
+
+  it("can make a 3 line string version of an empty grid", function() {
+    //Arrange
+    var ticTacToe = new TicTacToe();
+    //Act
+    var display = ticTacToe.displayGrid();
+    //Assert
+    assert.equal(display, "_|_|_\n_|_|_\n | | \n");
+  });
+
+  it("can make a 3 line string of a grid with an x in the center (2,2)", function() {
+    //Arrange
+    var ticTacToe = new TicTacToe();
+    //Act
+    ticTacToe.set(2, 2, "X");
+    var display = ticTacToe.displayGrid();
+    //Assert
+    assert.equal(display, "_|_|_\n_|X|_\n | | \n");
+  });
+
+  xit("can show x wins when x is at 1,1 1,2 and 1,3", function() {
+  });
+
 });
